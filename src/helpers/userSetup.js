baac399528573b71f0a33f905ee00664963f774c
Mikael Weiss <campingmikael@icloud.com>
2026-01-22 16:24:43 -0700

    Added better syntax highlighting

--- src/helpers/userSetup.js
+++ src/helpers/userSetup.js
@@ -1,6 +1,7 @@
+const highlightjs = require("markdown-it-highlightjs");
+
 function userMarkdownSetup(md) {
-  // The md parameter stands for the markdown-it instance used throughout the site generator.
-  // Feel free to add any plugin you want here instead of /.eleventy.js
+  md.use(highlightjs);
 }
 function userEleventySetup(eleventyConfig) {
   // The eleventyConfig parameter stands for the the config instantiated in /.eleventy.js.
